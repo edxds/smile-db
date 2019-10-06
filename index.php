4b8907fca5f85d278b29395ff83e8f61094e80e2
Eduardo Souza <edux404@gmail.com>
2019-10-06 15:31:10 -0300

    Add index.css stylesheet

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ if ($viewModel->shouldRedirectToConnect()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/index.css">
+
     <title>
       <?= $viewModel->pageTitle() ?>
     </title>
